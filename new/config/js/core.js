8df17198765a2c624cad3919d5ec596af159452e
Tagoon Prappre <tagoon.p@psu.ac.th>
2022-08-25 13:17:47 +0700

    20220825 1315

--- new/config/js/core.js
+++ new/config/js/core.js
@@ -1,2 +1,5 @@
-let api = 'http://localhost/sis/new/api/'
-let domain = 'http://localhost/sis/new/'
+// let api = 'http://localhost/sis/new/api/'
+// let domain = 'http://localhost/sis/new/'
+
+let api = 'https://medipe.app/sis/new/api/'
+let domain = 'https://medipe.app/sis/new/'
